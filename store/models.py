e37f80c369cd0821c803c1646562873746d1c8e4
Typowy99 <konradkunka99@gmail.com>
2024-02-16 13:26:46 +0100

    add product sales info

--- store/models.py
+++ store/models.py
@@ -25,6 +25,8 @@ class Product(models.Model):
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     description = models.CharField(max_length=300, default='', blank=True)
     image = models.ImageField(upload_to='uploads/product/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
 
     def __str__(self):
         return self.name
