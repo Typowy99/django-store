a7354b8113ffaff33beb16d006aef4dd0c3f14e2
Typowy99 <konradkunka99@gmail.com>
2024-02-18 13:29:30 +0100

    Add login/logout

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    #path('register/', views.register_user, name='register'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 def index(request):
     products = Product.objects.all()
@@ -10,3 +17,29 @@ def index(request):
 
 def about(request):
     return render(request, 'store/about.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in."))
+            return redirect('index')
+        else:
+            messages.success(request, ("There was an error, please try again!"))
+            return redirect('login')
+    else:
+        return render(request, "store/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, ("You have been logged out."))
+    return redirect('index')
